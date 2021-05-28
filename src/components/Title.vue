8a98844f49520f72edce46720d84efeb1291014c
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-05-28 14:21:26 +0200

    Vue CLI

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1> {{msg}} </h1>
+</template>
+
+<script>
+export default {
+    name: 'Tilte',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+
+
+<style scoped lang="scss">
+
+</style>
