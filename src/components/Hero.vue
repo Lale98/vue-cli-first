74b72397a6f355973ec52199d0f96f8b84106bad
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-05-28 14:40:51 +0200

    jumbotron

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="jumbotron"></div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style scoped lang="scss">
+    #jumbotron {
+        min-height: 500px;
+        background-image: url("../assets/logo.png");
+        background-size: 100px;
+    }
+</style>
